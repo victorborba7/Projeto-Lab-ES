22e3878e493a1bbb8d06494ff323d9600b5e9fc8
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-20 22:42:31 -0300

    Include business files

---
+++ src/main/java/business/EntidadeBO.java
@@ -0,0 +1,12 @@
+package business;
+
+import java.util.List;
+
+import org.fpij.jitakyoei.model.beans.Entidade;
+
+public interface EntidadeBO {
+	public void createEntidade(Entidade entidade) throws Exception;
+	public void updateEntidade(Entidade entidade) throws Exception;
+	public List<Entidade> searchEntidade(Entidade entidade) throws Exception;
+	public List<Entidade> listAll() throws Exception;
+}
---
+++ src/main/java/business/ProfessorEntidadeBO.java
@@ -0,0 +1,10 @@
+package business;
+
+import java.util.List;
+
+import org.fpij.jitakyoei.model.beans.ProfessorEntidade;
+
+public interface ProfessorEntidadeBO {
+	public void createProfessorEntidade(List<ProfessorEntidade> relacionamentos)
+	throws Exception;
+}
