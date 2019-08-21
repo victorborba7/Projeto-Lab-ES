c46e7965e88ffc2d2f5608b5f7526388fec657d9
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-20 22:44:34 -0300

    Include view files

---
+++ src/main/java/view/AppView.java
@@ -0,0 +1,9 @@
+package view;
+
+import org.fpij.jitakyoei.facade.AppFacade;
+
+public interface AppView {
+	public void handleModelChange(Object obj);
+	public void displayException(Exception e);
+	public void registerFacade(AppFacade facade);
+}
---
+++ src/main/java/view/ViewComponent.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.JPanel;
+
+import org.fpij.jitakyoei.facade.AppFacade;
+
+public interface ViewComponent {
+	public JPanel getGui();
+	public void registerFacade(AppFacade fac);
+}
