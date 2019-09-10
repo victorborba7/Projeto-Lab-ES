e7813d7aafe1903515d169eec31c3248dad71aa0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-09-10 18:58:49 -0300

    Include the front-end files

---
+++ src/main/webapp/js/change_divs.js
@@ -0,0 +1,62 @@
+function judocas(){
+    document.getElementById('bem_vindo').style.display=''; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function cadastrarDojo(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function administrarDojo(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function cadastrarProfessor(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function administrarProfessor(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function cadastrarAluno(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='';
+    document.getElementById('administrar_aluno').style.display='none';
+}
+
+function administrarAluno(){
+    document.getElementById('bem_vindo').style.display='none'; document.getElementById('cadastrar_dojo').style.display='none';
+    document.getElementById('administrar_dojo').style.display='none';
+    document.getElementById('cadastrar_professor').style.display='none';
+    document.getElementById('administrar_professor').style.display='none';
+    document.getElementById('cadastrar_aluno').style.display='none';
+    document.getElementById('administrar_aluno').style.display='';
+}
---
+++ src/main/webapp/js/sidebar_menu.js
@@ -0,0 +1,34 @@
+$("#menu-toggle-bem-vindo").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-cadastrar-dojo").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-administrar-dojo").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-cadastrar-professor").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-administrar-professor").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-cadastrar-aluno").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#menu-toggle-administrar-aluno").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
